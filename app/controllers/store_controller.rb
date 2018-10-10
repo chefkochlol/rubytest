f09de51512c9f39f95063d17ccde0611a4d9277f
Lukas Lauterbach <lukas.lauterback@njiuko.com>
2018-10-10 17:11:46 +0200

    cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,17 @@
 class StoreController < ApplicationController
+  before_action :increment_counter, only: [:index]
+
   def index
     @products = Product.order(:title)
+  end
+
+  private
+
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
   end
 end
